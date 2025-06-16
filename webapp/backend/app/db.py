2613e2f74d13256fe65358281e9d6d6ff0aa4e7e
César Esaú Flores Martínez <106702032+esauflores@users.noreply.github.com>
2025-06-16 16:36:34 +0700

    Mypy formatted

--- webapp/backend/app/db.py
+++ webapp/backend/app/db.py
@@ -1,9 +1,11 @@
-from typing import List
-from app.models import Movie, Review, RecommendationRequest
 import random
+
+from app.models import Movie, Review
+
 
 class MovieNotFoundError(Exception):
     pass
+
 
 # Mock database for testing purposes
 MOCK_MOVIES = [
@@ -69,79 +71,83 @@ MOCK_MOVIES = [
     ),
 ]
 
+
 class MockDB:
-    def get_movies_by_description(self, description: str, limit: int = 3) -> List[Movie]:
-        '''
+    def get_movies_by_description(
+        self, description: str, limit: int = 3
+    ) -> list[Movie]:
+        """
         Mock method to simulate fetching movies from a database based on description.
         This method randomly selects movies with limited results to simulate a database query.
-        
+
         Args:
             description (str): The description to filter movies by.
             limit (int): The maximum number of movies to return.
-        
+
         Returns:
             List[Movie]: A list of movies that match the description.
-        '''
-        
+        """
+
         selected_movies = random.sample(MOCK_MOVIES, min(limit, len(MOCK_MOVIES)))
         return selected_movies
-    
+
     def get_movie_by_id(self, movie_id: int) -> Movie:
-        '''
+        """
         Mock method to simulate fetching a movie by its ID.
-        
+
         Args:
             movie_id (int): The ID of the movie to fetch.
-        
+
         Returns:
             Movie: The movie with the specified ID, or None if not found.
-        '''
-        
+        """
+
         for movie in MOCK_MOVIES:
             if movie.id == movie_id:
                 return movie
 
         raise MovieNotFoundError(f"Movie with ID {movie_id} not found.")
-    
-    def get_reviews_by_movie_id(self, movie_id: int) -> List[Review]:
-        '''
+
+    def get_reviews_by_movie_id(self, movie_id: int) -> list[Review]:
+        """
         Mock method to simulate fetching reviews for a movie by its ID.
-        
+
         Args:
             movie_id (int): The ID of the movie to fetch reviews for.
-        
+
         Returns:
             List[Review]: A list of reviews for the specified movie, or an empty list if not found.
-        '''
-    
+        """
+
         movie = self.get_movie_by_id(movie_id)
         if movie:
             return movie.reviews
-        
+
         raise MovieNotFoundError(f"Movie with ID {movie_id} not found.")
-            
-    def get_all_movies(self) -> List[Movie]:
-        '''
+
+    def get_all_movies(self) -> list[Movie]:
+        """
         Mock method to simulate fetching all movies from the database.
-        
+
         Returns:
             List[Movie]: A list of all movies.
-        '''
-        
+        """
+
         return MOCK_MOVIES
-    
-    def get_movies_limit(self, limit: int = 3) -> List[Movie]:
-        '''
+
+    def get_movies_limit(self, limit: int = 3) -> list[Movie]:
+        """
         Mock method to simulate fetching a limited number of movies from the database.
-        
+
         Args:
             limit (int): The maximum number of movies to return.
-        
+
         Returns:
             List[Movie]: A list of movies limited to the specified count.
-        '''
-        
+        """
+
         return MOCK_MOVIES[:limit]
-    
+
+
 # Instantiate singleton mock DB connection
 mock_db: MockDB = MockDB()
